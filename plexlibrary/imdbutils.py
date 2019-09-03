7509ccbebb1b547a5a8342fd027ab4ef5ec32377
Gregory K. Spranger <gkspranger@Gregorys-MacBook-Pro.local>
2019-09-03 15:58:50 -0400

    good stuff

--- plexlibrary/imdbutils.py
+++ plexlibrary/imdbutils.py
@@ -89,32 +89,15 @@ class IMDb():
             if imdb_id in show_ids:
                 continue
 
-            if self.tvdb:
-                tvdb_data = self.tvdb.get_tvdb_from_imdb(imdb_id)
-
-            if self.tmdb:
-                tmdb_data = self.tmdb.get_tmdb_from_imdb(imdb_id, 'tv')
-
-            if tvdb_data and tvdb_data['firstAired'] != "":
-                year = datetime.datetime.strptime(tvdb_data['firstAired'],
-                                                  '%Y-%m-%d').year
-            elif tmdb_data and tmdb_data['first_air_date'] != "":
-                year = datetime.datetime.strptime(tmdb_data['first_air_date'],
-                                                  '%Y-%m-%d').year
-            elif imdb_years[i]:
-                year = str(imdb_years[i]).strip("()")
-            else:
-                year = datetime.date.today().year
+            tvdb_data = self._get_tvdb_data(imdb_id)
+            tmdb_data = self._get_tmdb_data(imdb_id)
+            year = self._get_show_year(tvdb_data, tmdb_data, imdb_years[i])
 
             # Skip old shows
-            if max_age != 0 \
-                    and (curyear - (max_age - 1)) > year:
+            if max_age != 0 and (curyear - (max_age - 1)) > year:
                 continue
 
-            if tvdb_data:
-                title = tvdb_data['seriesName']
-            else:
-                title = tmdb_data['name'] if tmdb_data else imdb_titles[i]
+            title = self._get_show_title(tvdb_data, tmdb_data, imdb_titles[i])
 
             show_list.append({
                 'id': imdb_id,
@@ -130,6 +113,48 @@ class IMDb():
                 show_ids.append('tvdb' + str(tvdb_data['id']))
 
         return show_list, show_ids
+
+    def _get_tvdb_data(self, imdb_id):
+        tvdb_data = None
+
+        if self.tvdb:
+            tvdb_data = self.tvdb.get_tvdb_from_imdb(imdb_id)
+
+        return tvdb_data
+
+    def _get_tmdb_data(self, imdb_id):
+        tmdb_data = None
+
+        if self.tmdb:
+            tmdb_data = self.tmdb.get_tmdb_from_imdb(imdb_id, 'tv')
+
+        return tmdb_data
+
+    @classmethod
+    def _get_show_year(cls, tvdb_data, tmdb_data, imdb_years):
+        if tvdb_data and tvdb_data['firstAired'] != "":
+            year = datetime.datetime.strptime( \
+                tvdb_data['firstAired'], '%Y-%m-%d').year
+        elif tmdb_data and tmdb_data['first_air_date'] != "":
+            year = datetime.datetime.strptime( \
+                tmdb_data['first_air_date'], '%Y-%m-%d').year
+        elif imdb_years:
+            year = str(imdb_years).strip("()")
+        else:
+            year = datetime.date.today().year
+
+        return year
+
+    @classmethod
+    def _get_show_title(cls, tvdb_data, tmdb_data, imdb_titles):
+        if tvdb_data:
+            title = tvdb_data['seriesName']
+        elif tmdb_data:
+            title = tmdb_data['name']
+        else:
+            title = imdb_titles
+
+        return title
 
     def add_items(self, item_type, url, item_list=None, item_ids=None,
                   max_age=0):
